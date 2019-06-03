932f228837ef471a4b62baea9f135fa6fd4242a0
HolyAngel <slverwolf@gmail.com>
2019-06-03 15:52:23 -0500

    build: Update system for anykernel3
    Huge thanks to Osm0sis for making this
    https://forum.xda-developers.com/showthread.php?t=2670512

--- build/anykernel.sh
+++ build/anykernel.sh
@@ -1,11 +1,11 @@
-# AnyKernel2 Ramdisk Mod Script
+# AnyKernel3 Ramdisk Mod Script
 # osm0sis @ xda-developers
-# blu_spark changes by eng.stk
+# Changes for SkyDragon by HolyAngel @ xda-developers
 
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=### SkyDragon Kernel for OnePlus 7 Pro ###
+kernel.string=SkyDragon Kernel for the OnePlus 7 Pro
 do.devicecheck=1
 do.modules=0
 do.cleanup=1
@@ -14,16 +14,18 @@ device.name1=OnePlus7Pro
 device.name2=guacamole
 device.name3=OnePlus 7 Pro
 device.name4=Guacamole
+device.name5=
+supported.versions=
 '; } # end properties
 
 # shell variables
-block=boot;
+block=/dev/block/bootdevice/by-name/boot;
 is_slot_device=1;
 ramdisk_compression=auto;
 
 ## AnyKernel methods (DO NOT CHANGE)
 # import patching functions/variables - see for reference
-. /tmp/anykernel/tools/ak2-core.sh;
+. tools/ak3-core.sh;
 
 ## Trim partitions
 fstrim -v /cache;
@@ -36,13 +38,17 @@ chown -R root:root $ramdisk/*;
 
 ## AnyKernel install
 dump_boot;
+
 # begin ramdisk changes
+
 if [ -d $ramdisk/.backup ]; then
   patch_cmdline "skip_override" "skip_override";
 else
   patch_cmdline "skip_override" "";
 fi;
-# end ramdisk changes
-write_boot;
 
+# end ramdisk changes
+
+write_boot;
 ## end install
+
